b81c9c5964a18ca233b282a3b83aa69ffadffbe5
Michael Christensen <Michael Christensen>
2016-08-11 13:32:57 +0100

    logs for debug

--- Votes.js
+++ Votes.js
@@ -5,8 +5,6 @@ var db = new AWS.DynamoDB.DocumentClient();
 exports.handler = function(event, context, callback) {
   console.log(JSON.stringify(event, null, 2));
   event.Records.forEach(function(record) {
-    console.log(record.eventID);
-    console.log(record.eventName);
     console.log('DynamoDB Record: %j', record.dynamodb);
 
     // Check which parameter has been changed (up/down)
@@ -19,11 +17,14 @@ exports.handler = function(event, context, callback) {
       down = record.dynamodb.NewImage.down - record.dynamodb.OldImage.down;
     }
 
+    console.log("VOTES: " + up + ", " + down);
+
     if(up > 0 || down > 0) {
       // fetch the dbTable from the event ARN of the form:
       // arn:aws:dynamodb:us-east-1:111111111111:table/test/stream/2020-10-10T08:18:22.385
       // see: http://stackoverflow.com/questions/35278881/how-to-get-the-table-name-in-aws-dynamodb-trigger-function
       var dbTable = event.eventSourceARN.split(':')[5].split('/')[1];
+      console.log("TABLE NAME: " + dbTable);
       // update the post's individual stat on this branch
       db.update({
         TableName: dbTable,
@@ -38,7 +39,11 @@ exports.handler = function(event, context, callback) {
           }
         }
       }, function(err, data) {
-        if(err) return callback(err);
+        if(err) {
+          console.log(err);
+          return callback(err);
+        }
+        console.log("SUCCESS: %j", data);
         return callback(null, "message");
       });
     }
