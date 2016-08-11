19567a229bdeb376f6767d42c33344626b5fc2a4
Michael Christensen <Michael Christensen>
2016-08-11 16:27:24 +0100

    wrapped async calls in promises so lambda not terminated prematurely

--- Votes.js
+++ Votes.js
@@ -4,6 +4,7 @@ var db = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function(event, context, callback) {
   console.log(JSON.stringify(event, null, 2));
+  var promises = [];
   event.Records.forEach(function(record) {
     console.log('DynamoDB Record: %j', record.dynamodb);
 
@@ -16,69 +17,85 @@ exports.handler = function(event, context, callback) {
     if(record.dynamodb.OldImage.up.N != record.dynamodb.NewImage.up.N ||
        record.dynamodb.OldImage.down.N != record.dynamodb.NewImage.down.N) {
       // update the post's individual stat on this branch
-      db.update({
-        TableName: dbTable,
-        Key: {
-          id: record.dynamodb.Keys.id.S,
-          branchid: record.dynamodb.Keys.branchid.S
-        },
-        AttributeUpdates: {
-          individual: {
-            Action: 'PUT',
-            Value: Number(record.dynamodb.NewImage.up.N) - Number(record.dynamodb.NewImage.down.N)
+      promises.push(new Promise(function(resolve, reject) {
+        db.update({
+          TableName: dbTable,
+          Key: {
+            id: record.dynamodb.Keys.id.S,
+            branchid: record.dynamodb.Keys.branchid.S
+          },
+          AttributeUpdates: {
+            individual: {
+              Action: 'PUT',
+              Value: Number(record.dynamodb.NewImage.up.N) - Number(record.dynamodb.NewImage.down.N)
+            }
           }
-        }
-      }, function(err, data) {
-        if(err) {
-          console.log(err);
-          return callback(err); // TODO: should we error out?
-        }
-        console.log("SUCCESS INDIVIDUAL");
-      });
+        }, function(err, data) {
+          if(err) {
+            console.log(err);
+            return reject(err); // TODO: should we error out?
+          }
+          resolve();
+        });
+      }));
     }
 
     // Update local stats if the individual stat has been updated
     if(record.dynamodb.OldImage.individual.N != record.dynamodb.NewImage.individual.N) {
       var inc = Number(record.dynamodb.NewImage.individual.N) - Number(record.dynamodb.OldImage.individual.N);
       console.log("INCREMENTING: " + inc);
-      // get the tags of this branch, which indicate all the branches above it in the tree
-      db.query({
-        TableName: dbTable,
-        KeyConditionExpression: "branchid = :id",
-        ExpressionAttributeValues: {
-          ":id": record.dynamodb.Keys.branchid.S
-        }
-      }, function(err, data) {
-        if(err) return callback(err);
-        if(!data || !data.Items) {
-          return callback('Error fetching branch tags');
-        }
+      promises.push(new Promise(function(resolve, reject) {
+        // get the tags of this branch, which indicate all the branches above it in the tree
+        db.query({
+          TableName: dbTable,
+          KeyConditionExpression: "branchid = :id",
+          ExpressionAttributeValues: {
+            ":id": record.dynamodb.Keys.branchid.S
+          }
+        }, function(err, data) {
+          console.log("LOCAL CALLBACK HERE!");
+          if(err) return reject(err);
+          if(!data || !data.Items) {
+            return reject('Error fetching branch tags');
+          }
 
-        console.log("UPDATING %j", data.Items);
-        // update the post's local stat on each tagged branch
-        data.Items.forEach(function(item) {
-          db.update({
-            TableName: dbTable,
-            Key: {
-              id: record.dynamodb.Keys.id.S,
-              branchid: item.tag
-            },
-            AttributeUpdates: {
-              local: {
-                Action: 'ADD',
-                Value: inc
-              }
-            }
-          }, function(err, data) {
-            if(err) {
-              console.log("ERROR LOCAL %j", err);
-              return callback(err); // TODO: should we error out?
-            }
-            console.log("SUCCESS LOCAL");
+          console.log("UPDATING %j", data.Items);
+          // update the post's local stat on each tagged branch
+          var updates = [];
+          data.Items.forEach(function(item) {
+            updates.push(new Promise(function(resolve, reject) {
+              db.update({
+                TableName: dbTable,
+                Key: {
+                  id: record.dynamodb.Keys.id.S,
+                  branchid: item.tag
+                },
+                AttributeUpdates: {
+                  local: {
+                    Action: 'ADD',
+                    Value: inc
+                  }
+                }
+              }, function(err, data) {
+                if(err) {
+                  console.log("ERROR LOCAL %j", err);
+                  return reject(err); // TODO: should we error out?
+                }
+                console.log("SUCCESS LOCAL");
+                resolve();
+              });
+            }));
           });
+          Promise.all(updates).then(resolve, reject);
         });
-      });
+      }));
     }
   });
-  callback(null, "Successfully updated stats!");
+  Promise.all(promises).then(function() {
+    console.log("WOOHOO UPDATED!");
+    callback(null, "Successfully updated stats!");
+  }, function() {
+    console.log("ERROR UPDATING");
+    callback("Error updating stats!");
+  });
 };
